560dce7063116e39b6a47457eaea1671f392c4d0
Logarithmus <opensource@logarithmus.dev>
2020-04-27 03:06:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# i.reddit.com-redirect
+Replaces all links to reddit.com with i.reddit.com, super-lightweight version of Reddit for mobile devices
+
+**Usage**
+1. Install [TamperMonkey](https://tampermonkey.net) extension into your browser.
+2. Go to [OpenUserJS](https://openuserjs.org/scripts/Logarithmus/i.reddit.com-redirect) and press "Install".
+3. Now try searching for reddit posts with your favourite search engine. Find a link to `reddit.com`. If you click it, you'll be forwarded to `i.reddit.com`
---
+++ i.reddit.com-redirect.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         i.reddit.com redirect
+// @version      0.1.5
+// @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
+// @homepageURL  https://github.com/Logarithmus/i.reddit.com-redirect
+// @updateURL    https://openuserjs.org/meta/Logarithmus/i.reddit.com_redirect.meta.js
+// @downloadURL  https://openuserjs.org/install/Logarithmus/i.reddit.com_redirect.user.js
+// @copyright    2020, Logarithmus (https://openuserjs.org/users/Logarithmus)
+// @description  Redirect to i.reddit.com
+// @author       Logarithmus
+// @match        *://*/*
+// ==/UserScript==
+
+(function() {
+    for (let a of document.getElementsByTagName("a")) {
+        // Only modify relevant <a> tags to avoid breaking sites
+        if (a.hostname === "reddit.com") {
+            a.hostname = "i.reddit.com"
+        }
+    }
+})();
